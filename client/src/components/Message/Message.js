3bbc9e195c012d6ce2f09d6bd2757c641ec769e3
Víctor De Abreu <55564718+Vicdeabreu@users.noreply.github.com>
2020-07-07 13:55:31 -0300

    Modificações no Message e no TextContainer

--- client/src/components/Message/Message.js
+++ client/src/components/Message/Message.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 import './Message.css';
 
-const Message = ({ message, name }) => {
+import ReactEmoji from 'react-emoji';
+
+const Message = ({ message: { user, text }, name }) => {
   let isSentByCurrentUser = false;
 
   const trimmedName = name.trim().toLowerCase();
@@ -14,10 +16,20 @@ const Message = ({ message, name }) => {
   return(
     isSentByCurrentUser
       ? (
-        // here
+        <div className="messageContainer justifyEnd">
+          <p className="sentText pr-10">{trimmedName}</p>
+          <div className="messageBox backgroundBlue">
+          <p className="messageText colorWhite">{ReactEmoji.emojify(text)}</p>
+          </div>
+        </div> 
       )
       : (
-        // here
+        <div className="messageContainer justifyStart">
+          <div className="messageBox backgroundLight">
+          <p className="messageText colorDark">{ReactEmoji.emojify(text)}</p>
+          </div>
+          <p className="sentText pl-10">{user}</p>
+        </div> 
       )
   )
 }
