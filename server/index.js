f85de0e960d3a2ca3e2eef6c625b43f63cf76328
Víctor De Abreu <55564718+Vicdeabreu@users.noreply.github.com>
2020-07-06 23:07:18 -0300

    Avances do día em Infobar e Input

--- server/index.js
+++ server/index.js
@@ -13,12 +13,28 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on('connection', (socket) => {
-  console.log('We have a new conection!!!');
 
   socket.on('join', ({ name, room }, callback) => {
-    console.log(name, room);
+    const { error,user } = addUser({ id: socket.id, name, room });
 
+    if(error) return callback(error);
+
+    socket.emit('message', { user: 'admin', text: `${user.name}, wellcome to the room ${user.room}`});
+    socket.broadcast.to(user.room).emit('message', { user : 'admin', text: `${user.name}, has joined!`});
+
+
+    socket.join(user.room);
+
+    callback();
   });
+
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit('message', { user: user.name, text: message });
+
+    callback();
+    });
 
   socket.on('disconnect', () => {
     console.log('User had left!!!');
